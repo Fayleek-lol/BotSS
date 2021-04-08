6bf53ff5fa729cc364c80e9d1bb76523aee2da5f
Stast <60179402+Stasyan4444@users.noreply.github.com>
2021-04-08 18:32:57 +0300

    Start

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import telebot
 bot = telebot.TeleBot('1756217731:AAFJZYYU9GrQezzgSrHCHOdZ1hvW0Cg5Zoo')
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
 keyboard1.row('Привет', 'Пока')
+keyboard1.row('Привет', 'Пока')
+keyboard1.row('Привет', 'Пока')
+
