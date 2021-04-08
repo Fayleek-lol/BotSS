23f947d85f5a514d02a746e4098e44bdb53f6595
Stast <60179402+Stasyan4444@users.noreply.github.com>
2021-04-08 22:44:32 +0300

    AboutMercury

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ keyboard1.row('Привет', 'Пока')
 keyboard2 = telebot.types.ReplyKeyboardMarkup(True, True)
 keyboard2.row('Планеты', 'Солнечная система', 'Спутники', 'Солнце', 'МКС', 'Пока')
 keyboard3 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard3.row('Меркурий', 'Венера', 'Земля', 'Марс', 'Юпитер', 'Сатурн', ' Уран', 'Нептун')
+keyboard3.row('Меркурий', 'Венера', 'Земля', 'Марс', 'Юпитер', 'Сатурн', ' Уран', 'Нептун', 'Пока')
 
 
 @bot.message_handler(commands=['start'])
@@ -36,6 +36,13 @@ def send_text(message):
                          f'прибудет с тобой сила!', reply_markup=keyboard1)
     elif message.text == "Планеты":
         bot.send_message(message.from_user.id, "О какой планете ты хочешь узнать?", reply_markup=keyboard3)
+    elif message.text == "Меркурий":
+        bot.send_message(message.from_user.id,
+                         "Меркурий — ближайшая к Солнцу планета Солнечной системы,наименьшая из планет земной группы. "
+                         "Названа в честь древнеримского бога торговли — быстрого Меркурия, "
+                         "поскольку она движется по небу быстрее других планет."
+                         " Её период обращения вокруг Солнца составляет всего 87,97 земных суток"
+                         " Это самый короткий среди всех планет Солнечной системы.")
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
