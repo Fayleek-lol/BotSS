bc02d759563b5d84346005f6a5819b70f26dcdc0
Fayleek-lol <72887964+Fayleek-lol@users.noreply.github.com>
2021-04-08 19:29:51 +0300

    ваващпхвазщпхзва

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import telebot
 
 bot = telebot.TeleBot('1756217731:AAFJZYYU9GrQezzgSrHCHOdZ1hvW0Cg5Zoo')
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard1.row('Привет', 'Планеты', 'Солнечная система', 'Спутники', 'Солнце', 'МКС', 'Пока')
+keyboard1.row('Привет', 'Пока')
+keyboard2 = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard2.row('Планеты', 'Солнечная система', 'Спутники', 'Солнце', 'МКС', 'Пока')
 
 
 @bot.message_handler(commands=['start'])
@@ -23,7 +25,7 @@ def send_text(message):
         bot.send_message(message.from_user.id, "Напиши Привет")
     elif message.text == "Пока":
         bot.send_message(message.from_user.id,
-                         "Прощай", reply_markup=keyboard1)
+                         "Прощай", reply_markup=keyboard2)
     elif message.text == "Планеты":
         bot.send_message(message.from_user.id, "О какой планете ты хочешь узнать?")
     else:
