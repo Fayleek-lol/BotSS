0e82546590879f56df2a3d5a0a432e449fa96c07
Fayleek-lol <72887964+Fayleek-lol@users.noreply.github.com>
2021-04-08 19:04:50 +0300

    fhdyhjtukj

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import telebot
 
 bot = telebot.TeleBot('1756217731:AAFJZYYU9GrQezzgSrHCHOdZ1hvW0Cg5Zoo')
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard1.row('Привет', 'Пока')
-keyboard2 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard1.row('Планеты', 'Солнечная система', 'Спутники', 'Солнце', 'МКС')
+keyboard1.row('Привет', 'Пока', 'Планеты', 'Солнечная система', 'Спутники', 'Солнце', 'МКС')
 
 
 @bot.message_handler(commands=['start'])
@@ -20,16 +18,13 @@ def send_text(message):
         bot.send_message(message.from_user.id,
                          "Привет! ")
         bot.send_message(message.from_user.id,
-                         "О чем ты хочещь узнать?")
+                         "О чем ты хочешь узнать?", reply_markup=keyboard1)
     elif message.text == "/help":
         bot.send_message(message.from_user.id, "Напиши Привет")
     elif message.text == "Пока":
         bot.send_message(message.from_user.id, "Прощай")
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
-
-    if bot.send_message(message.from_user.id, "О чем ты хочешь узнать?"):
-        bot.reply_to(message, " ", reply_markup=keyboard2)
 
 
 bot.polling(none_stop=True, interval=0)
